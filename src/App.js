73adee9fd3221fdc19b0caad851476a296e770b9
AJ Strock <strockaj320@gmail.com>
2022-10-22 23:18:28 -0400

    🎃 First Halloween Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import {Link} from 'react'
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="snowflakes" aria-hidden="true" >
+      <Card className='main-focus'>
+      <Card.Img className='main-focus-img' variant='bottom' src='./jackolantern.png'></Card.Img>
+        <Card.Body>
+          
+
+          <Card.Title className='main-focus-title'>TrpicalSky™</Card.Title>
+          <Card.Text className='main-foucs-body'>
+          This page will have all my contact information and links to current/previous projects that I have worked on or am currently working on.
+        </Card.Text>
+        
+        
+        </Card.Body>
+        <Card.Footer className='main-foucs-footer'> <a class="main-focus-a" href="https://github.com/TrpicalSky" rel="noreferrer">Github
+          </a>
+        </Card.Footer>
+      </Card>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+      <div class="snowflake">
+      🎃
+      </div>
+  </div>
   );
 }
 
